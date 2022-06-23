2d150b5c9ffd30d65ee216ad01c5f701ec92cfdd
Denisa Haka <denisa.haka@ymail.com>
2022-06-23 11:22:55 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+﻿//FizzBuzz
+
+//list numbers to 100
+for (let i = 1; i <= 100; i++) {
+  //if number is divisible by 5 or 3 prints FizzBuzz
+  if (i % 15 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 5 === 0) {
+    //if number % by 5 prints Buzz
+    console.log("Buzz");
+  } else if (i % 3 === 0) {
+    //if number % by 3 prints Fizz
+    console.log("Fizz");
+  } else {
+    //if neither prints the number
+    console.log(i);
+  }
+}
